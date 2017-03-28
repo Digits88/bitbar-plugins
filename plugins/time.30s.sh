84eb0f5d991afa892c81e0ad25b906004759f31f
Adrian Cooney <cooney.adrian@gmail.com>
2017-03-28 17:12:52 +0100

    Showing time since clock in

--- plugins/time.30s.sh
+++ plugins/time.30s.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+#
+#   Teamwork Projects Clock Manager
+#
+#       Clock in and clock out from Teamwork project right from the taskbar.
+#
+#   Requirements:
+#
+#       A config.json in the parent directory (see config.example.json) and an tw-auth
+#       cookie value for `teamwork.auth` property in the config file.
+#
+#   Dependencies:
+#
+#       jq      - brew install jq
+#       curl    - brew install curl
+#
 export PATH="/usr/local/bin:/usr/bin:$PATH"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TW_AUTH=$(jq -r '.teamwork.auth' $DIR/../config.json)
@@ -20,6 +35,26 @@ clock_out() {
     update_clock out
 }
 
+pretty_timestamp() {
+    local TIMESTAMP=$1
+    local DATE=${TIMESTAMP:0:10}
+    local TIME=${TIMESTAMP:11:8}
+    local TODAY_DATE=$(date +"%Y-%m-%d")
+    local YESTERDAY_DATE=$(date -v-1d +"%Y-%m-%d")
+
+    echo -n "${TIME:0:5} " # Print the timestamp e.g. 12:55
+
+    case $DATE in
+        $TODAY_DATE)        echo "today" ;;
+        $YESTERDAY_DATE)    echo "yesterday" ;;
+        *)                  echo $DATE ;;
+    esac
+}
+
+capitalize() {
+    echo $(tr '[:lower:]' '[:upper:]' <<< ${1:0:1})${1:1}
+}
+
 run() {
     local LATEST_CLOCKIN=$(get_latest_clockin)
     local USER_ID=$(echo $LATEST_CLOCKIN | jq -r '.userId')
@@ -27,9 +62,15 @@ run() {
     local CLOCK_STATUS=$([[ -z "$CLOCKOUT_TIME" ]] && echo "in" || echo "out")
     local INVERTED_CLOCK_STATUS=$([[ ! -z "$CLOCKOUT_TIME" ]] && echo "in" || echo "out")
 
-    echo ":clock1: $CLOCK_STATUS"
+    echo ":clock1: $(capitalize $CLOCK_STATUS)"
     echo "---"
-    echo "Clock $INVERTED_CLOCK_STATUS | bash=\"$DIR/$PLUGIN_NAME\" param1=clock_$INVERTED_CLOCK_STATUS terminal=false refresh=true"
+
+    if [[ "$CLOCK_STATUS" -eq "in" ]]; then
+        local CLOCKIN_TIME=$(echo $LATEST_CLOCKIN | jq -r .clockInDatetime)
+        echo "Clocked in since $(pretty_timestamp $CLOCKIN_TIME)."
+    fi
+
+    echo "Clock $(capitalize $INVERTED_CLOCK_STATUS) | bash=\"$DIR/$PLUGIN_NAME\" param1=clock_$INVERTED_CLOCK_STATUS terminal=false refresh=true"
     echo "Open Time Manager | href=https://digitalcrew.teamwork.com/index.cfm#people/$USER_ID/time"
 }
 
